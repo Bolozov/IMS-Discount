9300d4f5e43beb0dc451143110d75a38472e61ac
Haykel Rekik <hayk.rekik@gmail.com>
2022-09-24 03:39:33 +0000

    removed bold menu items

--- platform/themes/martfury/partials/menu.blade.php
+++ platform/themes/martfury/partials/menu.blade.php
@@ -1,7 +1,7 @@
 <ul {!! $options !!} style="text-align: center !important">
     @foreach ($menu_nodes as $key => $row)
         <li class="@if ($row->has_child) menu-item-has-children @endif @if ($row->css_class) {{ $row->css_class }} @endif @if ($row->active) current-menu-item @endif">
-            <a style=" font-weight: 500; font-size: 18px; " href="{{ url($row->url) }}" @if ($row->target !== '_self') target="{{ $row->target }}" @endif>
+            <a  href="{{ url($row->url) }}" @if ($row->target !== '_self') target="{{ $row->target }}" @endif>
                 @if ($row->icon_font) <i class="{{ trim($row->icon_font) }}"></i> @endif {{ $row->title }}
             </a>
             @if ($row->has_child)
