5628b7332149d0bbcd3889b10d6a72bb2787ad33
Haykel Rekik <hayk.rekik@gmail.com>
2022-09-27 17:57:00 +0000

    Footer Text Added With Link

--- platform/themes/martfury/partials/footer.blade.php
+++ platform/themes/martfury/partials/footer.blade.php
@@ -1,133 +1,154 @@
  <footer class="ps-footer">
-        <div class="ps-container">
-            <div class="ps-footer__widgets">
-                <aside class="widget widget_footer widget_contact-us">
-                    <h4 class="widget-title">{{ __('Contact us') }}</h4>
-                    <div class="widget_content">
-                        @if (theme_option('hotline'))
-                            <p>{{ __('Call us 24/7') }}</p>
-                            <h3>{{ theme_option('hotline') }}</h3>
-                        @endif
-                        <p>{{ theme_option('address') }} <br><a href="mailto:{{ theme_option('email') }}">{{ theme_option('email') }}</a></p>
-                        <ul class="ps-list--social">
-                            @for($i = 1; $i <= 10; $i++)
-                                @if(theme_option('social-name-' . $i) && theme_option('social-url-' . $i) && theme_option('social-icon-' . $i))
-                                    <li>
-                                        <a href="{{ theme_option('social-url-' . $i) }}"
-                                           title="{{ theme_option('social-name-' . $i) }}" style="color: {{ theme_option('social-color-' . $i) }}">
-                                            <i class="fa {{ theme_option('social-icon-' . $i) }}"></i>
-                                        </a>
-                                    </li>
-                                @endif
-                            @endfor
-                        </ul>
-                    </div>
-                </aside>
-                {!! dynamic_sidebar('footer_sidebar') !!}
-            </div>
-            @if (Widget::group('bottom_footer_sidebar')->getWidgets())
-                <div class="ps-footer__links" id="footer-links">
-                    {!! dynamic_sidebar('bottom_footer_sidebar') !!}
-                </div>
-            @endif
-            <div class="ps-footer__copyright text-center" style="display: block !important">
-                <p>{{ theme_option('copyright') }}</p>
-              
-            </div>
-        </div>
-    </footer>
+     <div class="ps-container">
+         <div class="ps-footer__widgets">
+             <aside class="widget widget_footer widget_contact-us">
+                 <h4 class="widget-title">{{ __('Contact us') }}</h4>
+                 <div class="widget_content">
+                     @if (theme_option('hotline'))
+                         <p>{{ __('Call us 24/7') }}</p>
+                         <h3>{{ theme_option('hotline') }}</h3>
+                     @endif
+                     <p>{{ theme_option('address') }} <br><a
+                             href="mailto:{{ theme_option('email') }}">{{ theme_option('email') }}</a></p>
+                     <ul class="ps-list--social">
+                         @for ($i = 1; $i <= 10; $i++)
+                             @if (theme_option('social-name-' . $i) && theme_option('social-url-' . $i) && theme_option('social-icon-' . $i))
+                                 <li>
+                                     <a href="{{ theme_option('social-url-' . $i) }}"
+                                         title="{{ theme_option('social-name-' . $i) }}"
+                                         style="color: {{ theme_option('social-color-' . $i) }}">
+                                         <i class="fa {{ theme_option('social-icon-' . $i) }}"></i>
+                                     </a>
+                                 </li>
+                             @endif
+                         @endfor
+                     </ul>
+                 </div>
+             </aside>
+             {!! dynamic_sidebar('footer_sidebar') !!}
+         </div>
+         @if (Widget::group('bottom_footer_sidebar')->getWidgets())
+             <div class="ps-footer__links" id="footer-links">
+                 {!! dynamic_sidebar('bottom_footer_sidebar') !!}
+             </div>
+         @endif
+         <div class="ps-footer__copyright text-center" style="display: block !important">
+             <p>
+                 © {{ \Carbon\Carbon::now()->year }} IMS DISCOUNT. Proudly Crafted with ❤️ by <a
+                     href="https://end2end.tn/" target="__blank">
+                     END 2 END DIGITAL
+                 </a>
+             </p>
 
-    @if (is_plugin_active('newsletter') && theme_option('enable_newsletter_popup', 'yes') === 'yes')
-        <div data-session-domain="{{ config('session.domain') ?? request()->getHost() }}"></div>
-        <div class="ps-popup" id="subscribe" data-time="{{ (int)theme_option('newsletter_show_after_seconds', 10) * 1000 }}" style="display: none">
-            <div class="ps-popup__content bg--cover" data-background="{{ RvMedia::getImageUrl(theme_option('newsletter_image')) }}" style="background-size: cover!important;"><a class="ps-popup__close" href="#"><i class="icon-cross"></i></a>
-                <form method="post" action="{{ route('public.newsletter.subscribe') }}" class="ps-form--subscribe-popup newsletter-form">
-                    @csrf
-                    <div class="ps-form__content">
-                        <h4>{{ __('Get 25% Discount') }}</h4>
-                        <p>{{ __('Subscribe to the mailing list to receive updates on new arrivals, special offers and our promotions.') }}</p>
-                        <div class="form-group">
-                            <input class="form-control" name="email" type="email"  placeholder="{{ __('Email Address') }}" required>
-                        </div>
+         </div>
+     </div>
+ </footer>
 
-                        @if (setting('enable_captcha') && is_plugin_active('captcha'))
-                            <div class="form-group">
-                                {!! Captcha::display() !!}
-                            </div>
-                        @endif
+ @if (is_plugin_active('newsletter') && theme_option('enable_newsletter_popup', 'yes') === 'yes')
+     <div data-session-domain="{{ config('session.domain') ?? request()->getHost() }}"></div>
+     <div class="ps-popup" id="subscribe"
+         data-time="{{ (int) theme_option('newsletter_show_after_seconds', 10) * 1000 }}" style="display: none">
+         <div class="ps-popup__content bg--cover"
+             data-background="{{ RvMedia::getImageUrl(theme_option('newsletter_image')) }}"
+             style="background-size: cover!important;"><a class="ps-popup__close" href="#"><i
+                     class="icon-cross"></i></a>
+             <form method="post" action="{{ route('public.newsletter.subscribe') }}"
+                 class="ps-form--subscribe-popup newsletter-form">
+                 @csrf
+                 <div class="ps-form__content">
+                     <h4>{{ __('Get 25% Discount') }}</h4>
+                     <p>{{ __('Subscribe to the mailing list to receive updates on new arrivals, special offers and our promotions.') }}
+                     </p>
+                     <div class="form-group">
+                         <input class="form-control" name="email" type="email"
+                             placeholder="{{ __('Email Address') }}" required>
+                     </div>
 
-                        <div class="form-group">
-                            <button class="ps-btn" type="submit" >{{ __('Subscribe') }}</button>
-                        </div>
-                        <div class="ps-checkbox">
-                            <input class="form-control" type="checkbox" id="dont_show_again" name="dont_show_again">
-                            <label for="dont_show_again">{{ __("Don't show this popup again") }}</label>
-                        </div>
-                    </div>
-                </form>
-            </div>
-        </div>
-    @endif
+                     @if (setting('enable_captcha') && is_plugin_active('captcha'))
+                         <div class="form-group">
+                             {!! Captcha::display() !!}
+                         </div>
+                     @endif
 
-    {!! Theme::get('bottomFooter') !!}
+                     <div class="form-group">
+                         <button class="ps-btn" type="submit">{{ __('Subscribe') }}</button>
+                     </div>
+                     <div class="ps-checkbox">
+                         <input class="form-control" type="checkbox" id="dont_show_again" name="dont_show_again">
+                         <label for="dont_show_again">{{ __("Don't show this popup again") }}</label>
+                     </div>
+                 </div>
+             </form>
+         </div>
+     </div>
+ @endif
 
-    <div id="back2top"><i class="icon icon-arrow-up"></i></div>
-    <div class="ps-site-overlay"></div>
-    @if (is_plugin_active('ecommerce'))
-        <div class="ps-search" id="site-search"><a class="ps-btn--close" href="#"></a>
-            <div class="ps-search__content">
-                <form class="ps-form--primary-search" action="{{ route('public.products') }}" data-ajax-url="{{ route('public.ajax.search-products') }}" method="get">
-                    <input class="form-control input-search-product" name="q" value="{{ request()->query('q') }}" type="text" autocomplete="off" placeholder="{{ __('Search for...') }}">
-                    <div class="spinner-icon">
-                        <i class="fa fa-spin fa-spinner"></i>
-                    </div>
-                    <button><i class="aroma-magnifying-glass"></i></button>
-                    <div class="ps-panel--search-result"></div>
-                </form>
-            </div>
-        </div>
-    @endif
-    <div class="modal fade" id="product-quickview" tabindex="-1" role="dialog" aria-labelledby="product-quickview" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content"><span class="modal-close" data-dismiss="modal"><i class="icon-cross2"></i></span>
-                <article class="ps-product--detail ps-product--fullwidth ps-product--quickview">
-                </article>
-            </div>
-        </div>
-    </div>
+ {!! Theme::get('bottomFooter') !!}
 
-    <script>
-        window.trans = {
-            "View All": "{{ __('View All') }}",
-            "No reviews!": "{{ __('No reviews!') }}",
-        }
-        window.siteUrl = "{{ route('public.index') }}";
-    </script>
+ <div id="back2top"><i class="icon icon-arrow-up"></i></div>
+ <div class="ps-site-overlay"></div>
+ @if (is_plugin_active('ecommerce'))
+     <div class="ps-search" id="site-search"><a class="ps-btn--close" href="#"></a>
+         <div class="ps-search__content">
+             <form class="ps-form--primary-search" action="{{ route('public.products') }}"
+                 data-ajax-url="{{ route('public.ajax.search-products') }}" method="get">
+                 <input class="form-control input-search-product" name="q" value="{{ request()->query('q') }}"
+                     type="text" autocomplete="off" placeholder="{{ __('Search for...') }}">
+                 <div class="spinner-icon">
+                     <i class="fa fa-spin fa-spinner"></i>
+                 </div>
+                 <button><i class="aroma-magnifying-glass"></i></button>
+                 <div class="ps-panel--search-result"></div>
+             </form>
+         </div>
+     </div>
+ @endif
+ <div class="modal fade" id="product-quickview" tabindex="-1" role="dialog" aria-labelledby="product-quickview"
+     aria-hidden="true">
+     <div class="modal-dialog modal-dialog-centered" role="document">
+         <div class="modal-content"><span class="modal-close" data-dismiss="modal"><i class="icon-cross2"></i></span>
+             <article class="ps-product--detail ps-product--fullwidth ps-product--quickview">
+             </article>
+         </div>
+     </div>
+ </div>
 
-    {!! Theme::footer() !!}
+ <script>
+     window.trans = {
+         "View All": "{{ __('View All') }}",
+         "No reviews!": "{{ __('No reviews!') }}",
+     }
+     window.siteUrl = "{{ route('public.index') }}";
+ </script>
 
-     @if (session()->has('success_msg') || session()->has('error_msg') || (isset($errors) && $errors->count() > 0) || isset($error_msg))
-         <script type="text/javascript">
-             $(document).ready(function () {
-                 @if (session()->has('success_msg'))
-                    window.showAlert('alert-success', '{{ session('success_msg') }}');
-                 @endif
+ {!! Theme::footer() !!}
 
-                 @if (session()->has('error_msg'))
-                    window.showAlert('alert-danger', '{{ session('error_msg') }}');
-                 @endif
+ @if (session()->has('success_msg') ||
+     session()->has('error_msg') ||
+     (isset($errors) && $errors->count() > 0) ||
+     isset($error_msg))
+     <script type="text/javascript">
+         $(document).ready(function() {
+             @if (session()->has('success_msg'))
+                 window.showAlert('alert-success', '{{ session('success_msg') }}');
+             @endif
 
-                 @if (isset($error_msg))
-                    window.showAlert('alert-danger', '{{ $error_msg }}');
-                 @endif
+             @if (session()->has('error_msg'))
+                 window.showAlert('alert-danger', '{{ session('error_msg') }}');
+             @endif
 
-                 @if (isset($errors))
-                     @foreach ($errors->all() as $error)
-                        window.showAlert('alert-danger', '{!! $error !!}');
-                     @endforeach
-                 @endif
-             });
-         </script>
-     @endif
-    </body>
-</html>
+             @if (isset($error_msg))
+                 window.showAlert('alert-danger', '{{ $error_msg }}');
+             @endif
+
+             @if (isset($errors))
+                 @foreach ($errors->all() as $error)
+                     window.showAlert('alert-danger', '{!! $error !!}');
+                 @endforeach
+             @endif
+         });
+     </script>
+ @endif
+ </body>
+
+ </html>
