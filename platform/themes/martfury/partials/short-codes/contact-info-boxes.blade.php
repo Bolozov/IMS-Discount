45d08132d22cae96c8d60c7d20ca4d7f48fb744b
Haykel Rekik <hayk.rekik@gmail.com>
2022-10-04 21:17:04 +0000

    WIP - incremental commit 🚧🚀

--- platform/themes/martfury/partials/short-codes/contact-info-boxes.blade.php
+++ platform/themes/martfury/partials/short-codes/contact-info-boxes.blade.php
@@ -5,16 +5,35 @@
         </div>
         <div class="ps-section__content">
             <div class="row">
-                @for ($i = 1; $i <= 6; $i++)
-                    @if (theme_option('contact_info_box_' . $i . '_title'))
-                        <div class="col-md-4 col-sm-6 col-12">
-                            <div class="ps-block--contact-info">
-                                <h4>{{ theme_option('contact_info_box_' . $i . '_title') }}</h4>
-                                <p><span class="d-block">{{ theme_option('contact_info_box_' . $i . '_subtitle') }}</span><span>{{ theme_option('contact_info_box_' . $i . '_details') }}</span></p>
-                            </div>
-                        </div>
-                    @endif
-                @endfor
+                {{-- @dd(theme_option('contact_info_box_1_subtitle')) --}}
+
+                <div class="col-md-4 col-sm-6 col-12">
+                    <div class="ps-block--contact-info">
+                        <h4>E-mail</h4>
+                        <p><span class="d-block">
+                                {{ theme_option('contact_info_box_1_subtitle') }}
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-6 col-12">
+                    <div class="ps-block--contact-info">
+                        <h4>Adresse</h4>
+                        <p><span class="d-block">
+                                {{ theme_option('contact_info_box_2_subtitle') }}
+                            </span>
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-6 col-12">
+                    <div class="ps-block--contact-info">
+                        <h4>Numéro de Téléphone</h4>
+                        <p>
+                            <span class="d-block">
+                                {{ theme_option('contact_info_box_1_details') }}
+                            </span>
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
